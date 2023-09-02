740dcf91d286612027a876173406a7894b947942
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-09-02 13:42:47 +0100

    add left bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+import { RightBarComponent } from './components/right-bar/right-bar.component';
+import { AvatarModule } from 'primeng/avatar';
+import { ChipModule } from 'primeng/chip';
+import { HomeComponent } from './components/right-bar/home/home.component';
+import { AboutComponent } from './components/right-bar/about/about.component';
+import { ServiceComponent } from './components/right-bar/service/service.component';
+import { ContactComponent } from './components/right-bar/contact/contact.component';
+import { ResumeComponent } from './components/right-bar/resume/resume.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftBarComponent,
+    RightBarComponent,
+    HomeComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AvatarModule,
+    ChipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
