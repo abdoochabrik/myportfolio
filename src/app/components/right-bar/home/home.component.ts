f06ed12dc633a93cd6fd80cc4df84e54def07403
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-10-04 18:52:15 +0100

    new version

--- src/app/components/right-bar/home/home.component.ts
+++ src/app/components/right-bar/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  downloadPDF() {
+    const downloadLink = document.getElementById('downloadLink');
+    downloadLink?.click();
+}
 }
